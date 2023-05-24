471deb3c59f3a9c46fc11ec12e111fa62738ccd0
rarissimaavis <ros.fortino@gmail.com>
2023-05-25 00:17:54 +0200

    da fixare

---
+++ Divide et impera/liscons.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int max(int a, int b, int c)
+{
+	if (a > b)
+	{
+		if (a > c)
+			return a;
+		else
+			return c;
+	}
+	else
+	{
+		if (b > c)
+			return b;
+		else
+			return c;
+	}
+}
+
+//complessità O(nlogn)
+int liscons(int A[], int l, int r) 
+{
+	if (l > r)
+		return 0;
+
+	if (l == r) 
+		return 1;
+
+	int c = (l+r)/2;
+	
+	int nl = liscons(A, l, c);
+	int nr = liscons(A, c+1, r);
+
+	int nc = 0, j = c;
+	if (A[c] < A[c+1])
+	{
+		nc++;
+		while (j > l && A[j] > A[j-1])
+		{
+			j--;
+			nc++;
+		}
+		j = c+1;
+		while (j < r && A[j] < A[j+1])
+		{
+			j++;
+			nc++;
+		}
+	}
+
+	return max(nl, nr, nc);
+}
+
+int main()
+{
+	int A[] = {5, 4, 3, 2, 3, 4, 5, 6, 3, 2};
+	int l = 0, r = 10;
+	printf("\nLa sottosequenza crescente più lunga ha lunghezza %d\n", liscons(A, l, r));
+	return 0;
+}
---
+++ Divide et impera/liscons2.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int max(int a, int b, int c)
+{
+	if (a > b)
+	{
+		if (a > c)
+			return a;
+		else
+			return c;
+	}
+	else
+	{
+		if (b > c)
+			return b;
+		else
+			return c;
+	}
+}
+
+//complessità O(n)
+int liscons(int A[], int l, int r) 
+{
+	if (l > r)
+		return 0;
+
+	if (l == r) 
+		return 1;
+
+	int i, j, c = (l+r)/2, nc = 0, nl = 0, nr = 0;
+
+	if (A[c] < A[c+1])
+	{
+		i = c;
+		while (i > l && A[i] > A[i-1])
+		{
+			i--;
+			nc++;
+		}
+		j = c+1;
+		while (j < r && A[j] < A[j+1])
+		{
+			j++;
+			nc++;
+		}
+	}
+
+	if (i > nc) 
+		nl = liscons(A, l, i-1);
+
+	if (r-j > nc)
+		nr = liscons(A, j+1, r);
+
+	return max(nl, nr, nc);
+}
+
+int main()
+{
+	int A[] = {5, 4, 3, 2, 3, 4, 5, 6, 3, 2};
+	int l = 0, r = 10;
+	printf("\nLa sottosequenza crescente più lunga ha lunghezza %d\n", liscons(A, l, r));
+	return 0;
+}
