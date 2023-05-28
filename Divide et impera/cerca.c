40b2ecf1cb93c06d8c5fc441518f0478a20529e0
rarissimaavis <ros.fortino@gmail.com>
2023-05-28 15:51:12 +0200

    /

---
+++ Divide et impera/cerca.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+//complessità O(n)
+int cerca(int A[], int x, int l, int r) 
+{
+	if (l > r)
+		return 0;
+
+	int c = (l+r)/2;
+	
+	if (A[c] == x)
+		return 1;
+
+	return cerca(A, x, l, c-1) + cerca(A, x, c+1, r);
+}
+
+int main()
+{
+	int A[] = {3, 4, 1, 2, 3, 1, 4, 2, 1, 4};
+	int l = 0, r = 10, x = 4;
+	printf("\nL'elemento x compare %d volte in A\n", cerca(A, x, l, r));
+	return 0;
+}
--- README.md
+++ README.md
@@ -7,5 +7,6 @@
 ## Divide et impera
 - massimo numero di occorrenze consecutive
 - trova x in un array non ordinato
+- conta occorrenze di x
 - sottosequenza crescente più lunga contigua
 - funzioni varie su alberi
