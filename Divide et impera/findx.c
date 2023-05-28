e624b3844d7214e77373623e8f36289c64074fde
rarissimaavis <ros.fortino@gmail.com>
2023-05-28 11:56:22 +0200

    /

--- Divide et impera/findx.c
+++ Divide et impera/findx.c
@@ -11,13 +11,19 @@ int findx(int A[], int x, int l, int r)
 	if (A[c] == x)
 		return c;
 	
-	return findx(A, x, l, c-1) + findx(A, x, c+1, r) + 1;
+	int xl = findx(A, x, l, c-1);
+	int xr = findx(A, x, c+1, r);
+
+	if (xl != -1 && xr != -1)
+		return xl;
+	
+	return xl + xr + 1;
 }
 
 int main()
 {
 	int A[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	int l = 0, r = 10, x = 77;
+	int l = 0, r = 10, x = 7;
 	printf("\nL'elemento x si trova all'indice %d\n", findx(A, x, l, r));
 	return 0;
 }
