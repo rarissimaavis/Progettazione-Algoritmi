4b8a0a0c2780dc87020ae9d007ff083eabf5f889
rarissimaavis <ros.fortino@gmail.com>
2023-05-30 00:14:47 +0200

    /

--- Divide et impera/maxocc2.c
+++ Divide et impera/maxocc2.c
@@ -29,36 +29,36 @@ int maxocc(int A[], int l, int r)
 	else if (l == r && A[l] != 2)
 		return 0;
 
-	int i, j, c = (l+r)/2, nc = 0, nl = 0, nr = 0;
+	int c = (l+r)/2, nc = 0, nl = 0, nr = 0, i = c, j = c;
 
 	if (A[c] == 2)
 	{
 		i = c;
-		while (i > l && A[i] == 2)
+		while (i >= l && A[i] == 2)
 		{
 			i--;
 			nc++;
 		}
 		j = c+1;
-		while (j < r && A[j] == 2)
+		while (j <= r && A[j] == 2)
 		{
 			j++;
 			nc++;
 		}
 	}
 
-	if (i > nc) 
+	if (l-i > nc) 
 		nl = maxocc(A, l, i-1);
 
 	if (r-j > nc)
-		nr = maxocc(A, j+1, r);
+		nr = maxocc(A, j, r);
 
 	return max(nl, nr, nc);
 }
 
 int main()
 {
-	int A[] = {2, 2, 3, 6, 2, 2, 2, 2, 3, 3};
+	int A[] = {2, 2, 3, 2, 2, 6, 2, 2, 3, 3};
 	int l = 0, r = 9;
 	printf("\nIl massimo numero di occorrenze consecutive di 2 è %d\n", maxocc(A, l, r));
 	return 0;
